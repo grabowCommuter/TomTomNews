3d3e874f5edb392c3947ec0eaabfd670f4ef0dd9
grabowCommuter <grabow.commuter@gmail.com>
2019-01-22 16:17:53 +0100

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,18 @@
+{
+  "moduleId" : "grabowCommuter/TomTomNews",
+  "title" : "TomTom News",
+  "subtitle" : "Traffic-News from TomTom",
+
+  "backButton" : true,
+  "enableGPS" : true,
+  "zoomControl" : false,
+  "screenLockRot" : false,
+  "reload" : true,
+
+  "shouldOverrideUrlLoading1" : "http",
+  "shouldOverrideUrlLoading2" : "https",
+
+  "loadUrl" : "https://web-commuter.firebaseapp.com/tomtom3.html?lat=#lat#&lng=#lng#&zoom=#zoom#",
+
+  "browserLaunchLink" : "https://web-commuter.firebaseapp.com/tomtom3.html?lat=#lat#&lng=#lng#&zoom=#zoom#"
+}
